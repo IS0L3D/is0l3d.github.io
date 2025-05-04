bc9ec2fc487b5ebe8f175df201449f129603ba77
IS0L3D <arruchaedu@gmail.com>
2025-05-03 22:42:37 -0600

    GoatCounter Analytics

---
+++ js/goat.counter.js
@@ -0,0 +1,7 @@
+(function() {
+    var script = document.createElement('script');
+    script.setAttribute('data-goatcounter', 'https://is0l3d.goatcounter.com/count');
+    script.async = true;
+    script.src = '//gc.zgo.at/count.js';
+    document.head.appendChild(script);
+})();
